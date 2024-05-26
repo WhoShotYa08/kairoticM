5fc09b65268148d4b517c0f7cc3af421ed755b95
WhoShotYa08 <80717684+WhoShotYa08@users.noreply.github.com>
2024-05-26 06:37:03 +0200

    creating the header

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -1,7 +1,36 @@
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import { FaUser, FaShoppingCart } from 'react-icons/fa';
+
 export default function Header() {
-    return(
-        <>
-            Header
-        </>
-    );
+  return (
+    <header>
+      <Navbar expand='md' collapseOnSelect>
+        <Container>
+          <LinkContainer to="/">
+            <Navbar.Brand>
+            <img src="/images/logo.png" alt="KairoticM's Logo"/>
+            </Navbar.Brand>
+          </LinkContainer>
+
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <FaShoppingCart />
+                </Nav.Link>
+              </LinkContainer>
+
+              <LinkContainer to="/login">
+                <Nav.Link>
+                  <FaUser />
+                </Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
 }
