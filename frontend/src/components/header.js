d33e96265bdbd770c51e5627036da12f2257f2b7
WhoShotYa08 <80717684+WhoShotYa08@users.noreply.github.com>
2024-05-26 08:03:02 +0200

    arranging the navigation panel accordingly

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -1,30 +1,56 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
-import { FaUser, FaShoppingCart } from 'react-icons/fa';
+
 
 export default function Header() {
   return (
     <header>
-      <Navbar expand='md' collapseOnSelect>
+      <Navbar expand="md" collapseOnSelect>
         <Container>
           <LinkContainer to="/">
             <Navbar.Brand>
-            <img src="/images/logo.png" alt="KairoticM's Logo"/>
+              <img src="/images/logo.png" alt="KairoticM's Logo" className="nav-logo"/>
             </Navbar.Brand>
           </LinkContainer>
 
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
-              <LinkContainer to="/cart">
+            <Nav className="mx-auto">
+              <LinkContainer to="/">
                 <Nav.Link>
-                  <FaShoppingCart />
+                  Home
                 </Nav.Link>
               </LinkContainer>
 
-              <LinkContainer to="/login">
+              <LinkContainer to="/about">
                 <Nav.Link>
-                  <FaUser />
+                  About
+                </Nav.Link>
+              </LinkContainer>
+
+              <LinkContainer to="/service">
+                <Nav.Link>
+                  Services
+                </Nav.Link>
+              </LinkContainer>
+
+              <LinkContainer to="/contacts">
+                <Nav.Link>
+                  Contacts
+                </Nav.Link>
+              </LinkContainer>
+            </Nav>
+
+            <Nav>
+              <LinkContainer to="/">
+                <Nav.Link className="btn btn-width">
+                  Sign Up
+                </Nav.Link>
+              </LinkContainer>
+
+              <LinkContainer to="/">
+                <Nav.Link className="btn btn-width btn-dark">
+                  Login
                 </Nav.Link>
               </LinkContainer>
             </Nav>
