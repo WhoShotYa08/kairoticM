712eca32e7de2f491168c6c69b9d1110b584ee76
WhoShotYa08 <80717684+WhoShotYa08@users.noreply.github.com>
2024-07-18 15:16:33 +0200

    created the user routes

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,27 @@
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import dotenv from 'dotenv';
+import express from "express";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
-const port = process.env.PORT
-
+const port = process.env.PORT;
 
 const app = express();
 
 //Body parser middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 //Cookie parser middleware
-app.use(cookieParser())
+app.use(cookieParser());
 
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
 
-app.listen(port, () => console.log(`Server is running on port ${port}`))
+app.use("/api/users", userRoutes);
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
