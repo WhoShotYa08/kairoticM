cd6e5bd29a6284e33c7697fca03f90c960b767ea
WhoShotYa08 <80717684+WhoShotYa08@users.noreply.github.com>
2024-07-21 09:36:38 +0200

    login funtionality, just need to change input fields

--- frontend/src/screens/SignUp.js
+++ frontend/src/screens/SignUp.js
@@ -1,11 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Loader from "../components/loader";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useLoginMutation } from "../slices/usersApiSlice";
+import { toast } from "react-toastify";
+import { setCredentials } from "../slices/authSlice";
 import { FaGooglePlus } from "react-icons/fa6";
 
 export default function SignUp() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("");
+
+  const  dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [login, {isLoading}] = useLoginMutation();
+
+  const {userInfo} = useSelector((state) => state.auth);
+
+  const { search } = useLocation();
+  const sp = new URLSearchParams(search);
+  const redirect = sp.get("redirect") || "/";
+
+  useEffect(() => {
+    if(userInfo) {
+      navigate(redirect);
+    }
+  }, [userInfo, redirect, navigate])
+
+  const submitHandler = async (e) =>{
+    e.preventDefault();
+
+    try {
+      const res = await login({email, password}).unwrap();
+
+      dispatch(setCredentials({...res}));
+
+      navigate(redirect)
+    } catch (error) {
+      toast.error(error?.data?.message || error.error);
+    }
+  }
   return (
     <div className="overflow-hidden" style={{ height: "100%" }}>
       <div className="row h-100 g-0 position-relative">
-        <div className="col-lg-3 h-100 border rounded-3" style={{ position: "relative" }}>
+        <div
+          className="col-lg-3 h-100 border rounded-3"
+          style={{ position: "relative" }}
+        >
           <div
             className=""
             style={{
@@ -19,10 +61,13 @@ export default function SignUp() {
               <h4 className="text-white fw-bold p-0 m-0 mx-1">
                 Tailored Engineering
               </h4>
-              <h4 className="fw-bold text-white p-0 my-1 mx-2"> Design Solutions.</h4>
+              <h4 className="fw-bold text-white p-0 my-1 mx-2">
+                {" "}
+                Design Solutions.
+              </h4>
               <p className="text-white fs-11 mt-3 mx-2">
-                Established after identifying a need to supply our Clients
-                with full turn key solution to meet their business and project
+                Established after identifying a need to supply our Clients with
+                full turn key solution to meet their business and project
                 execution objectives.
               </p>
             </div>
@@ -39,7 +84,18 @@ export default function SignUp() {
               <h4>Project & Construction Management Service.</h4>
             </div>
           </div>
-          <img src="images\hero-carousel\hero-carousel-3.svg" className="img-fluid animated hover-animation hoverAnim floatingImg" alt="hero" style={{ position: "absolute", top: 250, left: 60, width: "100%", height: "auto" }} />
+          <img
+            src="images\hero-carousel\hero-carousel-3.svg"
+            className="img-fluid animated hover-animation hoverAnim floatingImg"
+            alt="hero"
+            style={{
+              position: "absolute",
+              top: 250,
+              left: 60,
+              width: "100%",
+              height: "auto",
+            }}
+          />
         </div>
         <div className="col-lg-9 border rounded-end rounded-3">
           <div className="mx-5 py-4">
