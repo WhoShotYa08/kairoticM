bc85c1a72f5a59fc03d77c1d8a85e41ae1f4b711
WhoShotYa08 <80717684+WhoShotYa08@users.noreply.github.com>
2024-07-23 08:45:12 +0200

    shows when someone is logged in now by displaying their name

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -1,15 +1,39 @@
-import { Navbar, Nav, Container } from "react-bootstrap";
+import { Navbar, Nav, Container, Badge, NavDropdown } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
-
+import { useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { useLogoutMutation } from "../slices/usersApiSlice";
+import { logout } from "../slices/authSlice";
 
 export default function Header() {
+  const { userInfo } = useSelector((state) => state.auth);
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [logoutApiCall] = useLogoutMutation();
+
+  const logoutHandler = async () => {
+    try {
+      await logoutApiCall().unwrap();
+      dispatch(logout());
+      navigate("/login");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <header>
       <Navbar expand="md" collapseOnSelect>
         <Container>
           <LinkContainer to="/">
             <Navbar.Brand>
-              <img src="/images/logo.png" alt="KairoticM's Logo" className="nav-logo"/>
+              <img
+                src="/images/logo.png"
+                alt="KairoticM's Logo"
+                className="nav-logo"
+              />
             </Navbar.Brand>
           </LinkContainer>
 
@@ -41,19 +65,29 @@ export default function Header() {
               </LinkContainer>
             </Nav>
 
-            <Nav>
-              <LinkContainer to="/login">
-                <Nav.Link className="btn btn-width">
-                  Sign Up
-                </Nav.Link>
-              </LinkContainer>
+            {userInfo ? (
+              <NavDropdown title={userInfo.name} id="username">
+                <LinkContainer to={"/profile"}>
+                  <NavDropdown.Item>Profile</NavDropdown.Item>
+                </LinkContainer>
 
-              <LinkContainer to="/login">
-                <Nav.Link className="btn btn-width btn-login">
-                  <span className="btn-login-content">Login</span>
-                </Nav.Link>
-              </LinkContainer>
-            </Nav>
+                <NavDropdown.Item onClick={logoutHandler}>
+                  Logout
+                </NavDropdown.Item>
+              </NavDropdown>
+            ) : (
+              <Nav>
+                <LinkContainer to="/login">
+                  <Nav.Link className="btn btn-width">Sign Up</Nav.Link>
+                </LinkContainer>
+
+                <LinkContainer to="/login">
+                  <Nav.Link className="btn btn-width btn-login">
+                    <span className="btn-login-content">Login</span>
+                  </Nav.Link>
+                </LinkContainer>
+              </Nav>
+            )}
           </Navbar.Collapse>
         </Container>
       </Navbar>
